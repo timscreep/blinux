13e0a65daae23503ae0dcbde603c3b1a54a0c5ee
timscreep <81462085+timscreep@users.noreply.github.com>
2025-01-02 18:21:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 sudo dnf copr enable solopasha/hyprland
 sudo dnf copr enable atim/heroic-games-launcher
 sudo dnf copr enable wezfurlong/wezterm-nightly
